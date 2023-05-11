713c2263a790025a22d63cbea7a00195e69e7f19
Julian Tyl <julian.tyl@hotmail.com>
2023-05-11 13:30:20 +1000

    backtrack :(

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,60 @@
-class Blockchain():
-    def  __init__(self):
-        pass
+import hashlib
+import json
+import re
 
-    def set_on_new_block(self, on_new_block):
-        pass
+"""
+******************************************************************
+RIPPED STRAIGHT FROM ONE OF THE TUTS, MAY BE A GOOD STARTING POINT
+AND MAY JUST HAVE EVERYTHING WE NEED
+******************************************************************
+"""
+
+sender_valid = re.compile('^[a-fA-F0-9]{64}$')
+
+
+def validate_transaction(transaction: dict):
+    sender_validation = False
+    content_validation = False
+
+    if transaction.get('sender') and isinstance(transaction['sender'], str):
+        sender_validation = sender_valid.search(transaction['sender'])
+
+    if transaction.get('message') and isinstance(transaction['message'], str):
+        content_validation = len(transaction['message']) <= 70 and transaction['message'].isalnum()
+
+    return sender_validation and content_validation
+
+
+class Blockchain():
+    def __init__(self):
+        self.blockchain = []
+        self.pool = []
+        self.pool_limit = 3
+        self.new_block('0' * 64)
+
+    def new_block(self, previous_hash=None):
+        block = {
+            'index': len(self.blockchain) + 1,
+            'transactions': self.pool.copy(),
+            'previous_hash': previous_hash or self.blockchain[-1]['current_hash'],
+        }
+        block['current_hash'] = self.calculate_hash(block)
+        self.pool = []
+        self.blockchain.append(block)
+
+    def last_block(self):
+        return self.blockchain[-1]
+
+    def calculate_hash(self, block: dict):
+        block_object: str = json.dumps({k: block.get(k) for k in ['index', 'transactions', 'previous_hash']},
+                                       sort_keys=True)
+        block_string = block_object.encode()
+        raw_hash = hashlib.sha256(block_string)
+        hex_hash = raw_hash.hexdigest()
+        return hex_hash
+
+    def add_transaction(self, transaction):
+        if len(self.pool) < self.pool_limit:
+            self.pool.append(transaction)
+            return True
+        return False
--- node.py
+++ node.py
@@ -1,25 +1,71 @@
 from blockchain import *
+from _thread import *
+import _thread
+import threading
+import socket
+
 import cryptography.hazmat.primitives.asymmetric.ed25519 as ed25519
+
+IP = "127.0.0.1"
+
 
 def make_transaction(message: str, private_key: ed25519.Ed25519PrivateKey, nonce: int):
 	pass
 
+
+def on_new_client(c, addr, server):
+	# TODO: figure this out
+	# this whole function is designed to be run as a thread
+	# pretty much just constantly looks for data, we will decide what to do with it later
+
+	# nvm i dont think append is called here, it is called in setup elsewhere
+	while True:
+		print('Connection from', c.getsockname())
+		data_rev = c.recv(1024)
+		if not data_rev:
+			print("didn't get data")
+			break
+		print(data_rev.decode('utf-8'))
+		data = bytes('yo', encoding='utf-8')
+		c.sendall(data)
+	c.close()
+
+
 class RemoteNode():
 	def __init__(self, host, port) -> None:
-		pass
+		self.host = host
+		self.port = port
+
 
 	def transaction(self, transaction):
+		# wtf transaction is supposed to do, idk i will keep this function here for now tho
 		pass
+
 
 class ServerRunner():
 	def __init__(self, host, port, f) -> None:
+		self.port = port
+		self.host = host
+		self.f = f
 		self.blockchain = Blockchain()
+		self.RNList = []
 
 	def start(self):
-		pass
+		try:
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+				print("Server host names: ", IP, "Port: ", self.port)
+				s.bind((IP, self.port))  # Bind to the port
+				s.listen(5)
+				while True:
+					c, addr = s.accept()
+					_thread.start_new_thread(on_new_client, (c, addr, self))
+				s.close()
+		except:
+			print("Can't connect to the Socket")
 
 	def stop(self):
 		pass
 
 	def append(self, remote_node: RemoteNode):
-		pass
+		# I assume u add the remote nodes to a list
+		self.RNList.append(remote_node)
